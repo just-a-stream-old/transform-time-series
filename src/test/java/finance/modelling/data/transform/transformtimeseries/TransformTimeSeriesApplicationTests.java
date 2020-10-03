a89317a75581edcffc37f4378588fdb0277071a8
keirbailey <keirinvestment@gmail.com>
2020-10-03 13:44:48 +0100

    Initial project structure w/ git ignore

---
+++ src/test/java/finance/modelling/data/transform/transformtimeseries/TransformTimeSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.transform.transformtimeseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformTimeSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
