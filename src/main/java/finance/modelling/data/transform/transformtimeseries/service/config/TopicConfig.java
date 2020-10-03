234a46631f7f5aba08dd5f563ac9bb67c827b509
keirbailey <keirinvestment@gmail.com>
2020-10-03 13:53:13 +0100

    Added topic configuration to inject into service

---
+++ src/main/java/finance/modelling/data/transform/transformtimeseries/service/config/TopicConfig.java
@@ -0,0 +1,20 @@
+package finance.modelling.data.transform.transformtimeseries.service.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class TopicConfig {
+
+    private final String traceIdHeaderName;
+    private final String ingestEodTimeSeriesD1Topic;
+
+    public TopicConfig(
+            @Value("${spring.cloud.stream.kafka.streams.header.traceId}") String traceIdHeaderName,
+            @Value("${spring.cloud.stream.bindings.generateTimeSeriesD1DataModel-in-0.destination}") String ingestEodTimeSeriesD1Topic) {
+        this.traceIdHeaderName = traceIdHeaderName;
+        this.ingestEodTimeSeriesD1Topic = ingestEodTimeSeriesD1Topic;
+    }
+}
